3c2ff9d8cf0fa0f75190c09f2171c4209a807d19
nucleosynthesis <n.c.k.wardle@googlemail.com>
2025-06-05 13:34:47 +0100

    add extras for doing ee results

---
+++ params/kappas_resolved.py
@@ -0,0 +1,66 @@
+
+from collections import OrderedDict as od
+pois = od()
+
+pois["kappa_W"] = {
+   "factor":1.,
+   "multiplier":1,
+   "range":[0.5,1.5],
+   "nominal":1
+}
+
+pois["kappa_Z"] = {
+   "factor":1.,
+   "multiplier":1,
+   "range":[0.5,1.5],
+   "nominal":1
+}
+
+pois["kappa_b"] = {
+   "factor":1.,
+   "multiplier":1,
+   "range":[0.2,1.8],
+   "nominal":1
+}
+pois["kappa_c"] = {
+   "factor":1.,
+   "multiplier":1,
+   "range":[0,3],
+   "nominal":1
+}
+pois["kappa_s"] = {
+   "factor":1.,
+   "multiplier":1,
+   "range":[-50,300],
+   "nominal":1
+}
+pois["kappa_tau"] = {
+   "factor":1.,
+   "multiplier":1,
+   "range":[0.,3],
+   "nominal":1
+}
+pois["kappa_mu"] = {
+   "factor":1.,
+   "multiplier":1,
+   "range":[-5,8],
+   "nominal":1
+}
+pois["kappa_gamma"] = {
+   "factor":1.,
+   "multiplier":1,
+   "range":[0.,3],
+   "nominal":1
+}
+pois["kappa_glu"] = {
+   "factor":1.,
+   "multiplier":1,
+   "range":[0,3],
+   "nominal":1
+}
+pois["kappa_Zg"] = {
+   "factor":1.,
+   "multiplier":1,
+   "range":[0.,3.],
+   "nominal":1
+}
